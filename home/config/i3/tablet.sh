3d1beda7f95460945f6468c8467fb29a2b83bc25
kindagoose <62570891+kindagoose@users.noreply.github.com>
2022-03-19 17:24:17 +0300

    Add files via upload

---
+++ home/config/i3/tablet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sh -c 'xsetwacom --set 16 Rotate half'
+sh -c 'xsetwacom --set 17 Rotate half'
+sh -c 'xsetwacom --set "HUION Huion Tablet_HS611 Pad pad" Button 1 "key ctrl"'
+sh -c 'xsetwacom --set "HUION Huion Tablet_HS611 Pad pad" Button 2 "key shift"'
+sh -c 'xsetwacom --set "HUION Huion Tablet_HS611 Pad pad" Button 3 "key alt"'
+sh -c 'xsetwacom --set "HUION Huion Tablet_HS611 Pad pad" Button 8 "key ctrl z"'
+sh -c 'xsetwacom --set "HUION Huion Tablet_HS611 Pad pad" Button 9 "key ctrl shift z"'
+sh -c 'xsetwacom --set "HUION Huion Tablet_HS611 Pad pad" Button 10 "key ctrl a"'
+sh -c 'xsetwacom --set "HUION Huion Tablet_HS611 Pad pad" Button 11 "key ctrl shift a"'
+sh -c 'xsetwacom --set "HUION Huion Tablet_HS611 Pad pad" Button 12 "key q"'
+sh -c 'xsetwacom --set "HUION Huion Tablet_HS611 Pad pad" Button 13 "key backspace"'
+sh -c 'xsetwacom --set "HUION Huion Tablet_HS611 Pad pad" Button 14 "key esc"'
