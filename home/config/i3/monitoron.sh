b5cf772f3072a5f70859aafb776e5e92653c25f1
kindagoose <62570891+kindagoose@users.noreply.github.com>
2022-03-27 22:33:16 +0300

    Delete home/config/i3 directory

--- home/config/i3/monitoron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sh -c 'pactl load-module module-loopback latency_msec=1 source=alsa_output.pci-0000_07_00.4.analog-stereo.monitor sink=alsa_output.usb-Burr-Brown_from_TI_USB_Audio_CODEC-00.analog-stereo-output'
-sh -c 'pactl load-module module-loopback latency_msec=1 source=alsa_output.pci-0000_05_00.1.hdmi-stereo-extra1.monitor sink=alsa_output.usb-Burr-Brown_from_TI_USB_Audio_CODEC-00.analog-stereo-output'
-#sh -c 'pactl load-module module-null-sink sink_name=OBS-Discord sink_properties=device.description="OBS-Discord"'
-#sh -c 'pactl load-module module-remap-source source_name=OBS-Discord master=OBS-Discord.monitor'
-
