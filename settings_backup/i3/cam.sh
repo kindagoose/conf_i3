23433709db5a838532f78e1e7db1d5cf36df3f16
kndgs <62570891+kndgs@users.noreply.github.com>
2023-11-22 13:28:24 +0300

    Delete settings_backup directory

--- settings_backup/i3/cam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
-gphoto2 --stdout --capture-movie | ffmpeg -hwaccel nvdec -c:v mjpeg_cuvid -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
-
-exit 0
--- settings_backup/i3/taskset_er_pid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-getpid="$(pidof eldenring.exe)"
-
-eval taskset -cpa 0-11 $getpid
-
-exit 0
